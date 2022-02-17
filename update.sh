e0aed41dc7fcabc1accf65ddffa69530ff6bc1d5
Spek,D.S. van der (Daan) <d.s.vanderspek@students.uu.nl>
2022-02-17 20:48:24 +0100

    Update script smarter

--- update.sh
+++ update.sh
@@ -1,6 +1,21 @@
 cd Desktop
 git pull
 cd Audio
-wget http://daanvanderspek.nl/malmo/Audio/0.wav
-wget http://daanvanderspek.nl/malmo/Audio/1.wav
+
+if cmp --silent -- "versionGit" "versionFTP"; then
+  echo "No audio update needed"
+else
+  wget http://daanvanderspek.nl/malmo/Audio/0.wav
+  wget http://daanvanderspek.nl/malmo/Audio/1.wav
+  wget http://daanvanderspek.nl/malmo/Audio/2.wav
+  wget http://daanvanderspek.nl/malmo/Audio/3.wav
+  wget http://daanvanderspek.nl/malmo/Audio/4.wav
+  wget http://daanvanderspek.nl/malmo/Audio/5.wav
+  wget http://daanvanderspek.nl/malmo/Audio/6.wav
+  wget http://daanvanderspek.nl/malmo/Audio/7.wav
+  wget http://daanvanderspek.nl/malmo/Audio/8.wav
+  wget http://daanvanderspek.nl/malmo/Audio/9.wav
+  
+fi
+
 cd ..
