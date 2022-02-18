d1b110947c48820be87f46c07afbee0bf3c2a89e
Spek,D.S. van der (Daan) <d.s.vanderspek@students.uu.nl>
2022-02-18 11:30:59 +0100

    Added basic stepper for server.py, but accepting is done in Connection class and I want to change that

--- pythonScripts/server.py
+++ pythonScripts/server.py
@@ -4,55 +4,141 @@ import sys
 from queue import Queue
 from _thread import *
 
-ServerSocket = socket.socket()
-host = '0.0.0.0'
-port = 25001
-ThreadCount = 0
+connections = []
+ssw = 0
+prevssw=-1
+message = ""
+running = True
 
-qs = []
-maxI = int(sys.argv[1])
+class Connection:
+    def __init__(self, i):
+        self.i = i
+        self.ip = ""
+        self.q = Queue(maxsize = 2)
+        self.client = ""
+        self.connected = False
 
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
+    def accept(self, ServerSocket):
+        print(str(self.i)+" is accepting")
+        client, address = ServerSocket.accept()
+        self.connect(client,address)
+    def connect(self, client, address):
+        self.client = client
+        self.connected = True
+        self.ip = address[0]
+        print('Connected to: ' + self.ip)
+        start_new_thread(self.sendThread, ())
 
-print('Waiting for a Connection..')
-ServerSocket.listen(5)
+    def sendMessage(self, s):
+        print(str(self.i)+" is sending")
+        if self.connected:
+            self.q.put(s)
+
+    def sendThread(self):
+        self.client.send(str.encode(str(self.i)))
+        while self.connected:
+            self.client.sendall(str.encode(self.q.get()))
+        self.client.close()
 
 
-def threaded_client(i,q,connection):
-    sentLast = ""
-    connection.send(str.encode(str(i)))
-    while True:
-        connection.sendall(str.encode(q.get()))
-    connection.close()
+def acceptConnection(ServerSocket, x):
+    global connection
+    if(len(connections)<x+1):
+        connections.append(Connection(x))
+    else:
+        connections[x] = Connection(x)
+    client, address = ServerSocket.accept()
+    connections[x].connect(client, address)
 
-
-for x in range(0,maxI):
-    q = Queue(maxsize = 2)
-    qs.append(q)
-    Client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(threaded_client, (x,q, Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-
-def sendMessage(s):
-    for q in qs:
-            q.put(s)
-
-while True:
-    command = input().split()
-    if(command[0] == "q"):
-        sendMessage("Exiting")
-        break
-    if(command[0] == "s"):        
-        sendMessage(command[1])
-
-done = False
-while done:
-    for q in qs:
-        done = done and q.empty()
+def stepper():
+    global ssw
+    global prevssw
+    global ServerSocket
+    global connections
+    global message
+    global running
+    if(not ssw==prevssw):
+        print(ssw)
+        prevssw=ssw
+    
+    if(ssw == 0):
+        #Reset
+        connections = []
+        ssw=10
+    elif(ssw == 10):
+        #Start accepting connections
+        ServerSocket = socket.socket()
+        host = '0.0.0.0'
+        port = 25001
+        try:
+            ServerSocket.bind((host, port))
+            print('Waiting for a connection..')
+            ServerSocket.listen(5)
+            ssw=20
+        except socket.error as e:
+            print(str(e))
+            ssw=10000 #error 1
+            
+        maxI = int(sys.argv[1])
+        for x in range(0,maxI):
+            connections.append(Connection(x))
+    elif(ssw == 20):
+        #Now listening
+        for c in connections:
+            start_new_thread(c.accept,(ServerSocket, ))
+        ssw = 30
+    elif(ssw == 30):
+        #check if all connections are live
+        maxI = int(sys.argv[1])
+        done = True
+        for c in connections:
+            #print(str(c.i)+" is "+str(c.connected))
+            done = done and c.connected
         
-ServerSocket.close()
+        if(done):
+            ssw=100
+        else:
+            ssw=30
+    elif(ssw == 100):
+        #Init done, now listening to keyboard commands
+        command = input().split()
+        if(command[0] == "q"):
+            message = "Exiting"
+            ssw = 1000
+        if(command[0] == "s"):        
+            message = command[1]
+            ssw = 200
+    elif(ssw == 200):
+        #Send message
+        for c in connections:
+            c.sendMessage(message)
+        message = ""
+        ssw=500
+    elif(ssw == 500):
+        #Wait for everyone to send message
+        done = True
+        for c in connections:
+            done = done and c.q.empty()
+        
+        if(done):
+            ssw=100
+        else:
+            ssw=500
+        
+    elif(ssw == 1000):
+        #Send quitting message
+        for c in connections:
+            c.sendMessage(message)
+        message = ""
+        ssw=1100
+    elif(ssw == 1100):
+        #break down connections?
+        ServerSocket.close()
+        ssw=2000
+    elif(ssw==2000):
+        running=False
+    else:
+        ssw=0
+        
+while running:
+    stepper()
